994d7646658870bc2d46f97227178f8343daf75e
I-am-aabi <zainulabidnkp@gmail.com>
2022-11-20 17:33:49 +0530

    included time field

--- lib/widgets/other/textfield.dart
+++ lib/widgets/other/textfield.dart
@@ -1,31 +1,34 @@
 import 'package:flutter/material.dart';
 
 class MyTextField extends StatelessWidget {
-  const MyTextField({super.key, required this.hint});
+  MyTextField({super.key, required this.hint, required this.fieldcontroller});
   final String hint;
+  final TextEditingController fieldcontroller;
+  // allelements new allelements()
+
   @override
   Widget build(BuildContext context) {
     return TextField(
+      controller: fieldcontroller,
       style: TextStyle(color: Color.fromARGB(255, 54, 191, 121)),
-        decoration: InputDecoration(
-                hintText: hint,
-                hintStyle:
-                    const TextStyle(color: Color.fromARGB(100, 123, 220, 126)),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  borderSide: const BorderSide(
-                    color: Color.fromARGB(255, 54, 191, 121),
-                    width: 1.0,
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: const BorderSide(
-                    color: Colors.green,
-                    width: 2.0,
-                  ),
-                ),
-              ),
+      decoration: InputDecoration(
+        hintText: hint,
+        hintStyle: const TextStyle(color: Color.fromARGB(100, 123, 220, 126)),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: const BorderSide(
+            color: Color.fromARGB(255, 54, 191, 121),
+            width: 1.0,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          borderSide: const BorderSide(
+            color: Colors.green,
+            width: 2.0,
+          ),
+        ),
+      ),
     );
   }
 }
